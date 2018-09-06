a483e03b79fe0905b462dd530dd3f862fae7f900
hamossss <40631448+hamossss@users.noreply.github.com>
2018-09-06 15:31:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1646,7 +1646,6 @@ if (command == "za5") {
 });
 
 client.on("message", message => {
-	var prefix = "$";
 	var args = message.content.split(' ').slice(1); 
 	var msg = message.content.toLowerCase();
 	if( !message.guild ) return;
@@ -1712,26 +1711,7 @@ client.on('message', message => {
     }
 });
 
-var AsciiTable = require('ascii-data-table').default
-client.on('message', message =>{
 
-    if(message.content == "$roles"){
-        var 
-        ros=message.guild.roles.size,
-        data = [['Rank', 'RoleName']]
-        for(let i =0;i<ros;i++){
-            if(message.guild.roles.array()[i].id !== message.guild.id){
-         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
-        }}
-        let res = AsciiTable.table(data)
-
-        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
-    }
-});
- 
-client.on('ready', () => {
-	console.log('I am ready!'); 
-  });
 
 client.on('message', message => {
       if(message.content === prefix + "hchannel") {
