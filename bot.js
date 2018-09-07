0c1ec432183692110c13f637b0c1164feaabd16e
hamossss <40631448+hamossss@users.noreply.github.com>
2018-09-07 11:42:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1319,18 +1319,6 @@ client.on('message',function(message) {
 });
 
 
-   client.on('message', message => {
-            if(!message.channel.guild) return;
-let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('$bcall')){
- if (message.author.id !== '466425075487342615') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
- if(!message.author.id === '466425075487342615') return;
-message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
-client.users.forEach(m =>{
-m.sendMessage(args)
-})
-}
-});
 
 client.on('message', msg => {
  if (msg.content.startsWith(prefix + 'cal')) {
